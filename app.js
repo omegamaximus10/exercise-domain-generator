cbf982e50f73dd5fe217f06c026435b08e75e6ee
omegamaximus10 <bryan_2904@hotmail.com>
2021-07-10 10:55:31 +0000

    ejercicio finalizado

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console fjsdfp
+
+let pronoun = ['The','Our', ];
+let adj = ['great', 'big',"fabolus","amazing" ];
+let noun = ['jogger','racoon',"gator", "duck"];
+let extension =[".com", ".es",".uk",".nl",".us"]
+
+
+for(i = 0; i<pronoun.length; i++){
+
+    for( j =0; j<adj.length; j++){
+
+        for ( k=0; k <noun.length; k++){
+
+            for( l=0; l<extension.length; l++) {
+
+                console.log (pronoun[i]+ adj[j]+noun[k]+extension[l]);
+            }
+        }
+    }
+}
+
